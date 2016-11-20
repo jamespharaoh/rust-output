9337d873c7124991a91db86ed7ffc8fef206a874
James Pharaoh <james@pharaoh.uk>
2016-11-20 13:43:26 +0100

    handle ctrl-c and ctrl-z

--- src/rawconsole.rs
+++ src/rawconsole.rs
@@ -1,17 +1,32 @@
 use std::cmp;
 use std::io;
 use std::io::Stdout;
+use std::sync::mpsc;
+use std::sync::mpsc::Receiver;
+use std::sync::mpsc::Sender;
+use std::thread;
+use std::thread::JoinHandle;
 
-use termion;
-use termion::raw::IntoRawMode;
-use termion::raw::RawTerminal;
+use libc;
 
 use output::Output;
 
+use termion;
+use termion::AsyncReader;
+use termion::event::Key;
+use termion::input::TermRead;
+use termion::raw::IntoRawMode;
+use termion::raw::RawTerminal;
+
 pub struct RawConsole {
-	terminal: Option <RawTerminal <Stdout>>,
+
+	output: Option <RawTerminal <Stdout>>,
 	columns: u64,
 	status: Option <String>,
+
+	input_receiver: Receiver <Key>,
+	input_thread: JoinHandle <()>,
+
 }
 
 impl RawConsole {
@@ -19,7 +34,9 @@ impl RawConsole {
 	pub fn new (
 	) -> Option <RawConsole> {
 
-		let mut terminal =
+		// setup output
+
+		let mut output =
 			match io::stdout ().into_raw_mode () {
 
 			Ok (terminal) =>
@@ -40,17 +57,86 @@ impl RawConsole {
 
 		};
 
+		// setup input
+
+		let (input_sender, input_receiver) =
+			mpsc::channel ();
+
+		let input_thread =
+			thread::spawn (
+				move || {
+
+			Self::input_thread (
+				input_sender);
+
+		});
+
 		Some (
 			RawConsole {
-				terminal: Some (terminal),
+
+				output: Some (output),
 				columns: columns,
 				status: None,
+
+				input_receiver: input_receiver,
+				input_thread: input_thread,
+
 			}
 		)
 
 	}
 
+	fn input_thread (
+		sender: Sender <Key>,
+	) {
+
+		let stdin =
+			io::stdin ();
+
+		for key_result in stdin.keys () {
+
+			if let Ok (key) = key_result {
+
+				match key {
+
+					Key::Ctrl ('c') => {
+
+						unsafe {
+
+							libc::kill (
+								libc::getpid (),
+								libc::SIGINT);
+
+						}
+
+					},
+
+					Key::Ctrl ('z') => {
+
+						unsafe {
+
+							libc::kill (
+								libc::getpid (),
+								libc::SIGSTOP);
+
+						}
+
+					},
+
+					_ => {
+						// ignore
+					},
+
+				}
+
+			}
+
+		}
+
+	}
+
 }
+
 
 impl Output for RawConsole {
 
@@ -59,18 +145,17 @@ impl Output for RawConsole {
 		status: & str,
 	) {
 
-		let status =
-			& status [
-				0 .. cmp::min (
-					self.columns as usize,
-					status.len (),
-				)
-			];
+		let status: String =
+			status.chars ().take (
+				self.columns as usize,
+			).collect ();
 
 		if self.status.is_some () {
 
 			stderr! (
-				"\r\x1b[A\x1b[K{}\r\n",
+				"\r{}{}{}\r\n",
+				termion::cursor::Up (1),
+				termion::clear::CurrentLine,
 				status);
 
 		} else {
@@ -82,7 +167,7 @@ impl Output for RawConsole {
 		}
 
 		self.status =
-			Some (status.to_owned ());
+			Some (status);
 
 	}
 
@@ -94,7 +179,9 @@ impl Output for RawConsole {
 		if self.status.is_some () {
 
 			stderr! (
-				"\r\x1b[A\x1b[K{}\r\n{}\r\n",
+				"\r{}{}{}\r\n{}\r\n",
+				termion::cursor::Up (1),
+				termion::clear::CurrentLine,
 				message,
 				self.status.as_ref ().unwrap ());
 
@@ -115,7 +202,9 @@ impl Output for RawConsole {
 		if self.status.is_some () {
 
 			stderr! (
-				"\r\x1b[A\x1b[K");
+				"\r{}{}",
+				termion::cursor::Up (1),
+				termion::clear::CurrentLine);
 
 		}
 
