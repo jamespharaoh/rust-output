72c9fe2a938f272625283bb5f1f57b3bf84cbc22
James Pharaoh <james@pharaoh.uk>
2017-01-28 17:05:20 +0100

    add background thread to reduce overhead from frequent updates

--- src/output_state.rs
+++ src/output_state.rs
@@ -1,29 +1,88 @@
 use std::mem;
+use std::sync::Arc;
+use std::sync::Mutex;
+use std::sync::Weak;
+use std::sync::mpsc;
+use std::thread;
+use std::time::Duration;
 
 use backend::*;
 use output_log::*;
 
 pub struct OutputState {
+
 	backend: Option <Box <Backend>>,
+
 	logs: Vec <OutputLogInternal>,
 	next_log_id: u64,
+
+	background_join_handle: Option <thread::JoinHandle <()>>,
+	background_sender: Option <mpsc::Sender <()>>,
+
 	paused: bool,
 	changed: bool,
+
 }
 
 impl OutputState {
 
 	pub fn new (
 		backend: Option <Box <Backend>>,
-	) -> OutputState {
+		update_duration: Duration,
+	) -> Arc <Mutex <OutputState>> {
 
-		OutputState {
+		let real_self = OutputState {
+
 			backend: backend,
+
 			logs: Vec::new (),
 			next_log_id: 0,
+
+			background_join_handle: None,
+			background_sender: None,
+
 			paused: false,
 			changed: false,
+
+		};
+
+		let shared_self =
+			Arc::new (Mutex::new (
+				real_self,
+			));
+
+		{
+
+			let mut real_self =
+				shared_self.lock ().unwrap ();
+
+			let (background_sender, background_receiver) =
+				mpsc::channel ();
+
+			real_self.background_sender =
+				Some (background_sender);
+
+			{
+
+				let shared_self =
+					Arc::downgrade (
+						& shared_self);
+
+				real_self.background_join_handle = Some (
+					thread::spawn (move ||
+						Self::background_thread (
+							shared_self,
+							background_receiver,
+							update_duration,
+						)
+					)
+				);
+
+			}
+
 		}
+
+		shared_self
 
 	}
 
@@ -68,33 +127,7 @@ impl OutputState {
 		& mut self,
 	) {
 
-		if self.paused {
-
-			self.changed = true;
-
-		} else {
-
-			self.changed = false;
-
-			if let Some (ref mut backend) = self.backend {
-
-				backend.update (
-					& self.logs);
-
-			}
-
-			let logs_temp =
-				mem::replace (
-					& mut self.logs,
-					vec! []);
-
-			self.logs =
-				logs_temp.into_iter ().skip_while (
-					|log_internal|
-					log_internal.state () != OutputLogState::Running
-				).collect ();
-
-		}
+		self.changed = true;
 
 	}
 
@@ -112,9 +145,105 @@ impl OutputState {
 
 		self.paused = false;
 
-		if self.changed {
-			self.update_backend ()
+		self.update_backend_real ();
+
+	}
+
+	fn update_backend_real (
+		& mut self,
+	) {
+
+		if ! self.changed || self.paused {
+			return;
 		}
+
+		if let Some (ref mut backend) =
+			self.backend {
+
+			backend.update (
+				& self.logs);
+
+		}
+
+		let logs_temp =
+			mem::replace (
+				& mut self.logs,
+				vec! []);
+
+		self.logs =
+			logs_temp.into_iter ().skip_while (
+				|log_internal|
+				log_internal.state () != OutputLogState::Running
+			).collect ();
+
+		self.changed = false;
+
+	}
+
+	fn background_thread (
+		shared_state: Weak <Mutex <OutputState>>,
+		background_receiver: mpsc::Receiver <()>,
+		update_time: Duration,
+	) {
+
+		loop {
+
+			// wait a bit
+
+			match background_receiver.recv_timeout (
+				update_time) {
+
+				Ok (()) => (),
+
+				Err (mpsc::RecvTimeoutError::Timeout) => (),
+
+				Err (mpsc::RecvTimeoutError::Disconnected) => break,
+
+			}
+
+			// perform updates
+
+			if let Some (ref mut shared_state) =
+				shared_state.upgrade () {
+
+				let mut state =
+					shared_state.lock ().unwrap ();
+
+				state.update_backend_real ();
+
+			}
+
+		}
+
+	}
+
+}
+
+impl Drop for OutputState {
+
+	fn drop (
+		& mut self,
+	) {
+
+		// ask background thread to stop
+
+		let background_sender =
+			self.background_sender.take ().unwrap ();
+
+		drop (background_sender);
+
+		// wait for background thread to stop
+
+		let background_join_handle =
+			self.background_join_handle.take ().unwrap ();
+
+		background_join_handle.join ().unwrap ();
+
+		// perform final update
+
+		self.paused = false;
+
+		self.update_backend_real ();
 
 	}
 
